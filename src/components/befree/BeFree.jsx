81429d6d7aa594f1080da735580990daf2b0b43a
nicobonder <nicobonder@hotmail.com>
2023-06-03 01:20:16 -0300

    Starting to create responsive desing

--- src/components/befree/BeFree.jsx
+++ src/components/befree/BeFree.jsx
@@ -3,19 +3,19 @@ import { motion } from "framer-motion"
 
 export default function BeFree() {
   return (
-    <section className='section  bg-[#48A167]'>
+    <section className='section bg-[#48A167] items-center flex justify-center'>
         <motion.div
-            className='h-80 w-[33%] bg-[#8a2be2] place-items-center flex flex-col justify-center'
+            className='h-50 md:h-52 lg:h-70 w-[33%] lg:w-[25%] bg-[#8a2be2] items-center flex flex-col justify-center px-1 py-1'
            
-            animate={{ opacity: [0.5, 1 ], x: ['-100%', '200%']}}
+            animate={{ opacity: [0.5, 1 ], x: ['-150%', '100%']}}
             transition={{ duration: 2, ease: 'easeInOut' }}
         >
             <motion.p
-                className='text-white text-center text-lg font-bold pt-2'
+                className='text-white text-center text-sm md:text-lg font-bold pt-2'
             >I&apos;m going to start moving every time the page loads </motion.p>
             <br />
              <motion.p
-                className='text-white text-center text-lg font-bold'
+                className='text-white text-center text-sm md:text-lg font-bold pt-2'
             >and you can&apos;t do anything to stop it...</motion.p>
         </motion.div>
     </section>
--- src/components/bird/Bird.jsx
+++ src/components/bird/Bird.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 export default function Bird() {
   return (
-    <section className='px-[1rem] h-[80vh] bg-[#48A167] py-[1rem]'>
+    <section className='px-[1rem] h-[100vh] bg-[#48A167] py-[1rem]'>
       <div className="">
         <h1 className='text-lg md:text-2xl font-bold mb-1'>
           Ok, now you can fell free
@@ -13,22 +13,24 @@ export default function Bird() {
           <span className='text-white'>(without touching the eagle)</span> ?
         </h2>
       </div>
-    <div  className='flex justify-center'>
+    
+    <div className='flex justify-center mt-8 md:mt-4'>
     <motion.img
           src='/assets/eagle.png'
           alt='An eagle attacking the bird'
-          className='h-64 w-64 mt-2 ml-24'
+          className='h-36 md:h-48 lg:h-60 md:w-48 w-36 lg:w-60 mt-4 lg:ml-24'
           drag
           whileTap={{
             cursor: "grabbing",
           }}
         />
     </div>
-      <div className='flex justify-between -mt-14'>
+
+      <div className='flex justify-between mt-20 md:mt-4 lg:-mt-14'>
         <motion.img
           src='/assets/bird.png'
           alt='A bird that you can move'
-          className='h-48 w-48 justify-start'
+          className='h-28 w-28 md:h-36 lg:h-40 md:w-36 lg:w-40 justify-start'
           drag
           whileTap={{
             cursor: "grabbing",
@@ -37,7 +39,7 @@ export default function Bird() {
         <img
           src='/assets/branch.png'
           alt='A branch'
-          className='h-64 w-58 justify-end'
+          className='h-40 md:h-52 lg:h-64 w-36 md:w-52 lg:w-58 justify-end'
         />
       </div>
     </section>
